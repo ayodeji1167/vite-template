afeeb265b877ef3355d159436c7ea617d91087f7
ayodeji1167 <afuwapeayodeji2018@gmail.com>
2024-05-01 05:55:55 +0100

    reconfigured provider

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,9 +1,26 @@
 import { ReactNode } from 'react';
 import { ThemeProvider } from './theme-provider';
+import { RecoilRoot } from 'recoil';
+import { HelmetProvider } from 'react-helmet-async';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/lib/reactQuery';
 
-type AppProviderProps = {
-  children: ReactNode;
-};
+const helmetContext = {};
+type AppProviderProps = { children: ReactNode };
 export function AppProvider({ children }: AppProviderProps) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <HelmetProvider context={helmetContext}>
+      <ThemeProvider>
+        <QueryClientProvider client={queryClient}>
+          {/* <ReactQueryDevtools
+            client={queryClient}
+            position="right"
+            buttonPosition="bottom-right"
+          /> */}
+          <RecoilRoot>{children}</RecoilRoot>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </HelmetProvider>
+  );
 }
