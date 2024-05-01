c0ee60eb58384b7559accac37e75b221e66e8ae1
ayodeji1167 <afuwapeayodeji2018@gmail.com>
2024-05-01 05:54:42 +0100

    added react query configurations

---
+++ src/lib/reactQuery/index.ts
@@ -0,0 +1,46 @@
+import {
+  QueryClient,
+  DefaultOptions,
+  UseQueryOptions,
+  UseMutationOptions,
+} from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+
+const queryConfig: DefaultOptions = {
+  queries: {
+    useErrorBoundary: false,
+    refetchOnWindowFocus: false,
+    retry(failureCount, error: any) {
+      if (error.status === 404) return false;
+      else if (failureCount < 2) return true;
+      else return false;
+    },
+    staleTime: 300000, // 5 minutes - Adjust for your application's needs.
+    cacheTime: 3600000, // 1 hour - Adjust for your application's needs.
+  },
+};
+
+export const queryClient = new QueryClient({
+  defaultOptions: queryConfig,
+});
+
+// This is used to extract the Return TYPE of a FUNCTION
+export type ExtractFnReturnType<FnType extends (...args: any) => any> = Awaited<
+  ReturnType<FnType>
+>;
+
+// This is used to get the queryConfigType of a specific QUERY function
+export type QueryConfigType<QueryFnType extends (...args: any) => any> = Omit<
+  UseQueryOptions<ExtractFnReturnType<QueryFnType>>,
+  'queryKey' | 'queryFn'
+>;
+
+// This is used to get the queryConfigType of a specific MUTATION function
+export type MutationConfig<MutationFnType extends (...args: any) => any> =
+  UseMutationOptions<
+    ExtractFnReturnType<MutationFnType>,
+    AxiosError,
+    Parameters<MutationFnType>[0]
+  >;
+
+export { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
