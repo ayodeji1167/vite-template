c34df673f6c51df85f315a95230da2188f4dc0cc
ayodeji1167 <afuwapeayodeji2018@gmail.com>
2024-05-01 05:24:14 +0100

    reconfigured husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { Suspense } from 'react';
 import './App.css';
-import { Canvas } from '@react-three/fiber';
 
 function App() {
   return (
-    <Canvas id="my-canvas">
+    <div id="my-canvas">
       <Suspense fallback={null}></Suspense>
-    </Canvas>
+    </div>
   );
 }
 
