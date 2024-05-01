2536a6410adf0cc3a72cbbff9fcdfd23c5f6d555
ayodeji1167 <afuwapeayodeji2018@gmail.com>
2024-05-01 05:22:22 +0100

    added env constant file

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Suspense } from 'react';
 import './App.css';
 import { Canvas } from '@react-three/fiber';
-import Three from './components/three/Three';
 
 function App() {
   return (
     <Canvas id="my-canvas">
-      <Suspense fallback={null}>
-        <Three />
-      </Suspense>
+      <Suspense fallback={null}></Suspense>
     </Canvas>
   );
 }
---
+++ src/shared/constants/env.ts
@@ -0,0 +1,5 @@
+export const env = {
+  API_BASE_URL: import.meta.env.VITE_APP_API_BASE_URL + '/v3',
+  GOOGLE_API_KEY: import.meta.env.VITE_APP_GOOGLE_API_KEY,
+  FRONTEND_URL: import.meta.env.VITE_APP_FRONTEND_URL,
+};
